c8585f3fa23d2a821db0e2112dc98f8b4d8335fa
hinsoned <102074110+hinsoned@users.noreply.github.com>
2023-12-27 16:48:03 -0500

    Some initial set up
    Import pygame, add loop, create screen

--- snake.py
+++ snake.py
@@ -2,4 +2,32 @@ import pygame
 
 pygame.init()
 
-#create an empty game window
+#create dimensions for an empty game window 
+screen_width = 600
+screen_height = 600
+
+#create the game window
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Snake')
+
+bg = (255, 200, 150)
+
+def draw_screen():
+    screen.fill(bg)
+
+#create game loop
+run = True
+
+while run:
+
+    draw_screen()
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    #update the display
+    pygame.display.update()
+
+#end pygame
+pygame.quit
